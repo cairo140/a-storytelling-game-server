01ac9b575f32070b7c6c8f2d3b114562dc8c4181
Steven Xu <steven@stevenxu.ca>
2014-01-17 22:42:16 -0700

    votes requested state transition and logging improvements.

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ Game.idIncrementer = 0;
 Game.FULL = 'FULL';
 Game.PLAYER_JOINED = 'PLAYER_JOINED';
 Game.SUBMISSION_RECEIVED = 'SUBMISSION_RECEIVED';
+Game.VOTES_REQUESTED = 'VOTES_REQUESTED';
 Game.prototype.players = null;
 Game.prototype.pastRounds = null;
 Game.prototype.currentRound = null;
@@ -33,6 +34,9 @@ Game.prototype.addPlayer = function(player) {
   if (this.players.length === FULL_GAME_SIZE) {
     this.emit(Game.FULL);
   }
+};
+Game.prototype.getNumExpectedSubmissions = function() {
+  return this.players.length;
 };
 Game.prototype.getState = function(player) {
   return {
@@ -141,31 +145,36 @@ AStorytellingGameServer.on('connection', function(ws) {
         currentGame = AStorytellingGameServer.pendingGame;
         ws.send(JSON.stringify({
           code: 'currentPlayerUpdate',
-          player: currentPlayer.getState()
+          player: currentPlayer.getState(currentPlayer)
         }));
         currentGame.on(Game.PLAYER_JOINED, function() {
           ws.send(JSON.stringify({
             code: 'playerJoined',
-            game: currentGame.getState()
+            game: currentGame.getState(currentPlayer)
           }));
         });
         currentGame.on(Game.SUBMISSION_RECEIVED, function() {
           ws.send(JSON.stringify({
             code: 'submissionReceived',
-            game: currentGame.getState()
+            game: currentGame.getState(currentPlayer)
+          }));
+        });
+        currentGame.on(Game.VOTES_REQUESTED, function() {
+          ws.send(JSON.stringify({
+            code: 'vote',
+            game: currentGame.getState(currentPlayer)
           }));
         });
         currentGame.on(Game.FULL, function() {
           ws.send(JSON.stringify({
             code: 'submit',
             message: 'Please submit your content. Send a response like {"code":"submitResponse","content":"Dr. Frankenstein was busy at work."}',
-            game: currentGame.getState()
+            game: currentGame.getState(currentPlayer)
           }));
         });
         currentGame.addPlayer(currentPlayer);
         break;
       case 'submitResponse':
-        console.log(currentGame.currentRound.submissions);
         if (currentGame.currentRound.submissions.some(function(s) { return s.player === currentPlayer })) {
           ws.send(JSON.stringify({
             code: 'submitRejected',
@@ -176,7 +185,13 @@ AStorytellingGameServer.on('connection', function(ws) {
           submission.player = currentPlayer;
           submission.content = messageObj.content;
           currentGame.currentRound.submissions.push(submission);
+          log('Submission received by %s: %s', currentPlayer.name, submission.content);
           currentGame.emit(Game.SUBMISSION_RECEIVED);
+          // FIXME: move elsewhere?
+          if (currentGame.currentRound.submissions.length === currentGame.getNumExpectedSubmissions()) {
+            console.log('Game %d advancing to voting round.', currentGame.id);
+            currentGame.emit(Game.VOTES_REQUESTED);
+          }
         }
         break;
       case 'voteResponse':
